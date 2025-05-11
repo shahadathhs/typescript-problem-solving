20f02b68199136ea454f9e8704e3ec5a24b0dc4c
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-11 21:35:11 +0600

    feat: problem 9 completed

--- src/app/sorting-objects.ts
+++ src/app/sorting-objects.ts
@@ -33,3 +33,29 @@ export const sortCarsByYearDesc = (cars: TCar[]) => {
   // * return
   return sortedCars
 }
+
+
+// * without sort method
+export const sortCarsByYearAsc2 = (cars: TCar[]) => {
+  const shadowCars = [...cars]
+
+  // * steps
+  // loop over cars array
+  // loop over cars array again inside the previous loop, starting from one index ahead
+  // now compare year of car a (from first loop) with year of car b (from second loop, also starting from one index ahead)
+  // if year of car a is greater than year of car b, swap them
+  for (let i = 0; i < shadowCars.length; i++) {
+    for (let j = i + 1; j < shadowCars.length; j++) {
+      if (shadowCars[i].year > shadowCars[j].year) {
+        const temp = shadowCars[i]
+        shadowCars[i] = shadowCars[j]
+        shadowCars[j] = temp
+      }
+    }
+  }
+
+  console.info('Given cars: ', cars, '\n')
+  console.info('Sorted cars by year asc: ', shadowCars, '\n')
+
+  return shadowCars
+}
